ef845ef05b64defbfa2bef618a169745be662df8
talitanasciment <142513164+talitanasciment@users.noreply.github.com>
2023-08-31 14:41:33 -0300

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -35,17 +35,15 @@ def main():
     x = int(input("Digite o valor 1: "))
     y = int(input("Digite o valor 2: "))
 
-    # if x > y:
-    #     x, y = y, x
-    # numeros = []
-    # for i in range(x+1, y):
-    #     if i % 5 == 2 or i % 5 == 3:
-    #         numeros.append(i)
+  
+    if x > y:
+        x, y = y, x
+    numeros = []
+    for i in range(x+1, y):
+        if i % 5 == 2 or i % 5 == 3:
+            numeros.append(i)
 
-    arr = divisao(x,y)
-
-
-    print("Os números são..: {}".format(" ".join(str(n) for n in arr)))
+    print("Os números são..: {}".format(" ".join(str(n) for n in numeros)))
 
 
 if __name__ == '__main__':
