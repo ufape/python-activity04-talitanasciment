d32634301438bb6acc02c5f727c6bf6e6dbb6fc4
talitanasciment <142513164+talitanasciment@users.noreply.github.com>
2023-08-31 18:29:42 +0000

    atualizar

--- problem_2.py
+++ problem_2.py
@@ -34,15 +34,7 @@ def main():
     
     x = int(input("Digite o valor 1: "))
     y = int(input("Digite o valor 2: "))
-
-  
-    if x > y:
-        x, y = y, x
-    numeros = []
-    for i in range(x+1, y):
-        if i % 5 == 2 or i % 5 == 3:
-            numeros.append(i)
-
+    
     arr = divisao(x,y)
 
     print("=-=-=-=-=-=-=-=-=-=")
